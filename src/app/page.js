10204ce4ad263389001b1ba549e27b5ebac1d7a5
ismail en-niou <132809252+ismail-en-niou@users.noreply.github.com>
2025-06-10 13:52:22 +0000

    first3

--- src/app/page.js
+++ src/app/page.js
@@ -21,21 +21,6 @@ const ParticlesBackground = () => {
         <motion.div
           key={i}
           className="absolute w-2 h-2 bg-black/5 rounded-full"
-          initial={{ 
-            x: Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight
-          }}
-          animate={{
-            x: Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight,
-            scale: [1, 1.5, 1],
-            opacity: [0.3, 0.8, 0.3]
-          }}
-          transition={{
-            duration: Math.random() * 10 + 10,
-            repeat: Infinity,
-            ease: "linear"
-          }}
         />
       ))}
     </div>
